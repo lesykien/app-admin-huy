47f0fd4226f164894dbd930971adbc3476ff94c6
lesykien <lesykien030@gmail.com>
2024-05-24 11:11:57 +0700

    get value form

--- src/app/components/home/add-product/add-product.component.ts
+++ src/app/components/home/add-product/add-product.component.ts
@@ -4,30 +4,43 @@ import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrl: './add-product.component.scss'
+  styleUrl: './add-product.component.scss',
 })
 export class AddProductComponent implements OnInit {
-
-  constructor (
-    private form : FormBuilder
-  ){}
-
+  constructor(private form: FormBuilder) {}
   productForm = this.form.group({
-    name : ['', Validators.required],
-    price : ['', Validators.required],
-    size : ['', Validators.required],
-    descrition : [''],
-    category : ['' , Validators.required], 
-    files : ['', Validators.required]
-  })
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    size: ['', Validators.required],
+    descrition: [''],
+    category: ['', Validators.required],
+    // file này chỉ dùng để kiểm lỗi cho form
+    file: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  Files: File[] = [];
 
-  }
+  ngOnInit(): void {}
 
-  SumbitForm(){
-    let valueForm : any = this.productForm.value;
+  SumbitForm() {
+    let valueForm: any = this.productForm.value;
+    // thông tin sản phẩm để gửi về serve
     console.log(valueForm);
+    // thông tin hình ảnh để gửi về serve
+    console.log(this.Files);
   }
 
+  // lấy thông tin hình ảnh
+  onFilesSelected(event: any) {
+    this.Files = [];
+    const files: FileList = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.Files.push(file);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
--- src/app/components/home/edit-product/edit-product.component.ts
+++ src/app/components/home/edit-product/edit-product.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-product',
   templateUrl: './edit-product.component.html',
   styleUrl: './edit-product.component.scss'
 })
-export class EditProductComponent {
+export class EditProductComponent implements OnInit {
+  constructor(private form: FormBuilder) {}
+  productForm = this.form.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    size: ['', Validators.required],
+    descrition: [''],
+    category: ['', Validators.required],
+    // file này chỉ dùng để kiểm lỗi cho form
+    file: ['', Validators.required],
+  });
 
+  Files: File[] = [];
+
+  ngOnInit(): void {}
+
+  SumbitForm() {
+    let valueForm: any = this.productForm.value;
+    // thông tin sản phẩm để gửi về serve
+    console.log(valueForm);
+    // thông tin hình ảnh để gửi về serve
+    console.log(this.Files);
+  }
+
+  // lấy thông tin hình ảnh
+  onFilesSelected(event: any) {
+    this.Files = [];
+    const files: FileList = event.target.files;
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.Files.push(file);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  // lấy dữ liều từ api ra form
+  RenderForm(response : any){
+    // this.productForm.get('name')!.setValue( --truyền dữ liều vào đây--- );
+  }
 }
